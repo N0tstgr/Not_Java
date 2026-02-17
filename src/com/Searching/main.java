9d9151ae75b29a8ddd79e0ad23714c0fa34d34e1
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-02-17 16:00:25 +0530

    element

--- src/com/Searching/main.java
+++ src/com/Searching/main.java
@@ -3,9 +3,11 @@ package com.Searching;
 public class main {
     public static void main(String[] args) {
         int[] num = {34,90,45,32,21,98,65};
-        int aimed = 21;
+        int aimed = 45;
 
-        System.out.println(LinearSearch(num,aimed));
+//        System.out.println(LinearSearch(num,aimed));
+        System.out.println(LinearSearch2(num,aimed));
+
 
     }
     public static int LinearSearch(int[] arr, int aimed){
@@ -14,13 +16,30 @@ public class main {
         if(a.length==0){
             return -1;
         }
-        for(int index =0; index<a.length; index++ ){
-           int element = a[index];
+        for(int element:a){
+
            if(element==aimed){
-               return index;
+               return element;
            }
 
         }
         return -1;
     }
+    public static int LinearSearch2(int[] arr, int aimed){
+        int [] a = arr;
+
+        if(a.length==0){
+            return -1;
+        }
+        for(int index = 0; index<a.length; index++){
+            int element = arr[index];
+
+            if(element==aimed){
+                return index;
+            }
+
+        }
+        return -1;
+    }
+
 }
