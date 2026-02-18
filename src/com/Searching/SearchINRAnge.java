807d8aa700341251b1caefa89dc8948ce5354908
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-02-18 19:22:08 +0530

    range

--- src/com/Searching/SearchINRAnge.java
+++ src/com/Searching/SearchINRAnge.java
@@ -3,17 +3,17 @@ package com.Searching;
 public class SearchINRAnge {
     public static void main(String[] args) {
         int[] arr = {2,5,6,7,9,22,21};
-        int aimed = 9;
-        int ans = range(arr,aimed);
+        int aimed = 29;
+        int ans = range(arr,aimed,3,6);
         System.out.println(ans);
 
 
     }
-    public static int range(int[] arr, int aimed){
+    public static int range(int[] arr, int aimed, int start, int stop){
         if(arr.length==0){
             return -1;
         }
-        for(int a = 3; a<5; a++){
+        for(int a = start; a<stop; a++){
             if(arr[a]==aimed){
                 return a;
             }
