98275738116e063d669eccc8845e1ff34064be9b
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-02-18 19:45:34 +0530

    attempting-min

---
+++ src/com/Searching/FindMinimum.java
@@ -0,0 +1,19 @@
+package com.Searching;
+
+public class FindMinimum {
+    public static void main(String[] args) {
+        int[] arr = {4,6,22,90,76,43,21};
+        System.out.println(Min(arr));
+
+    }
+    public static int Min(int[] arr){
+        int min = 0;
+        if(arr.length==0){
+            return -1;
+        }
+        for(int i = 0; i<arr.length; i++){
+            if()
+        }
+
+    }
+}
