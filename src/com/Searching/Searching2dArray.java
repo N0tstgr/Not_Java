5549dd8aaaf1a99b1d6a4ca544b796847e796ec8
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-02-20 21:50:14 +0530

    indexing-2dArray

--- src/com/Searching/Searching2dArray.java
+++ src/com/Searching/Searching2dArray.java
@@ -1,28 +1,29 @@
 package com.Searching;
-
+import java.util.Arrays;
 public class Searching2dArray {
     public static void main(String[] args) {
         int[][] arr = {
                 {3,5,2,55 },
                 {90,56,43,21,87,93},
-//                {44,77,88,99,00,},
+                {44,77,88,99,00},
                 { 49,67,32,78},
                 {101,102,103,104 }
         };
-        int target = 93;
-        System.out.println(Search2d(arr,target));
+        int target = 99;
+        int[] ans = Search2d(arr,target);
+        System.out.println(Arrays.toString(ans));
 
     }
-    public static int Search2d(int[][] arr, int target){
+    public static int[] Search2d(int[][] arr, int target){
          int aimed = target;
         for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<i; j++){
+            for(int j = 0; j<arr[i].length; j++){
             if(arr[i][j]==aimed) {
-                return arr[i][j];
+                return new int[]{i,j};
               }
 
             }
         }
-        return -1;
+        return new int[]{-1,-1};
     }
 }
