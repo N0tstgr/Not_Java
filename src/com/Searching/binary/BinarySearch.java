f53eba9c4cb9a8528312b7b2b67178825ed091ec
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-03-05 12:28:18 +0530

    corrected

--- src/com/Searching/binary/BinarySearch.java
+++ src/com/Searching/binary/BinarySearch.java
@@ -2,7 +2,7 @@ package com.Searching.binary;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {-1,-2,6,9,30,22,54,549};
+        int[] arr = {-1,-2,6,9,22,30,54,549};
         int target = 549;
         int ans = binarySearch(arr,target);
         System.out.println(ans);
@@ -18,9 +18,9 @@ public class BinarySearch {
             //find the middle element
             //int mid = (start+end)/2 //might be possible that (start+end) exceed the range of the int
             if(target<arr[mid]) {
-                end = start - 1;
+                end = mid - 1;
             }if(target>arr[mid]){
-                    start = end-1;
+                    start = mid+1;
                 }else{
                 //ans
                 return mid;
